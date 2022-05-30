f9b1a06dd73dc75df9b39ca64b389e42d0350a32
Deendayal Sundaria <deendayal.sundaria@macaw.nl>
2022-05-30 23:54:48 +0200

    changes

--- UmbracoTraining/App_Data/Models/all.generated.cs
+++ UmbracoTraining/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "c4e80b0846ba34bc")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.7")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
